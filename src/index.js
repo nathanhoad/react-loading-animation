df37c55a90abf35c9b0331e70e5d9c8fc5ebd5af
Nathan Hoad <nathan@nathanhoad.net>
2017-06-14 11:47:30 +1000

    Added support for specifying what kind of element is rendered for the spinner (defaulting to 'div')

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ const animation = `@keyframes rotate {
 
 class Loading extends React.Component {
     render () {
-        let { isLoading, children } = this.props;
+        let { component, className, isLoading, children } = this.props;
 
         if (isLoading) {
             let { width, height, margin, style } = this.props;
@@ -76,37 +76,68 @@ class Loading extends React.Component {
             loading_style.height = height;
             loading_style.margin = margin;
 
-            return (
-                <div style={Object.assign({}, loading_style, style)}>
-                    <style>{animation}</style>
-                    <svg style={svg_style} viewBox="25 25 50 50">
-                        <circle style={circle_style} cx="50" cy="50" r="20" fill="none" strokeWidth="7" strokeMiterlimit="10"/>
-                    </svg>
-                </div>
+            return React.createElement(
+                component,
+                { style: Object.assign({}, loading_style, style) },
+                <style>{animation}</style>,
+                <svg style={svg_style} viewBox="25 25 50 50">
+                    <circle style={circle_style} cx="50" cy="50" r="20" fill="none" strokeWidth="7" strokeMiterlimit="10"/>
+                </svg>
             );
         } else {
-            return (<div>{children || null}</div>);
+            return React.createElement(component, { className }, children || null);
         }
     }
 }
 
 
 Loading.propTypes = {
+    className: PropTypes.string,
     isLoading: PropTypes.bool,
     style: PropTypes.object,
     width: PropTypes.string,
     height: PropTypes.string,
-    margin: PropTypes.string
+    margin: PropTypes.string,
+    component: PropTypes.any
 };
 
 
 Loading.defaultProps = {
+    className: '',
     isLoading: true,
     style: {},
     width: '40px',
     height: '40px',
-    margin: '0 auto'
+    margin: '0 auto',
+    component: 'div'
 };
 
 
 module.exports = Loading;
+
+
+// Polyfills
+if (typeof Object.assign != 'function') {
+    Object.assign = function(target, varArgs) { // .length of function is 2
+        'use strict';
+        if (target == null) { // TypeError if undefined or null
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
+
+            if (nextSource != null) { // Skip over if undefined or null
+                for (var nextKey in nextSource) {
+                    // Avoid bugs when hasOwnProperty is shadowed
+                    if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+        }
+        return to;
+    };
+}
